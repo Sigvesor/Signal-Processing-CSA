9380baea5460fbeab2ad30c42f0833157c9af25c
alfredo <alfredo@alf.dd>
2020-01-29 14:54:32 +0100

    added bearing and stf(stator turns faults) visualization

--- run_fault_analyzer.py
+++ run_fault_analyzer.py
@@ -6,11 +6,17 @@ Created on Mon Jan 22 13:34:43 2020
 
 from FaultAnalyzer import FaultAnalyzer
 
+fault_display = [
+    'bearing',
+    'stf',
+]
+
 tmp = FaultAnalyzer(run_fft=False)
-# tmp.plot_fault_in_one(
-#     item='ic',
-#     x_limit=100,
-#     fault_vib_label=False,)
-    # data_names=['h', 'f1'],
+tmp.plot_fault_in_one(
+    item='ic',
+    x_limit=100,
+    fault_display=fault_display,)
+    # fault_vib_label=True,)
+#     data_names=['h', 'f1'],
 # )
-tmp.plot_all_faults()
+# tmp.plot_all_faults()
